d10815011e1052b598ef804673cd0b3e58ff07df
Jessica <chj19831207@163.com>
2014-10-28 23:35:38 +1100

    This is the final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## `makeCacheMatrix`: This function creates a special "matrix" object
+that can cache its inverse.
 
-## Write a short comment describing this function
+## The first function, `makeCacheMatrix` creates a special "matrix", which is
+really a list containing a function to
+
+1.  set the value of the matrix
+2.  get the value of the matrix
+3.  set the value of the inverse
+4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
-                x <<- y
-                m <<- NULL
+                x <<- y #set y to a global variable x
+                m <<- NULL #set a null global variable m 
         }
-        get <- function() x
-        setmatrix<- function(solve) m <<- solve
-        getmatrix <- function() m
+        get <- function() x # get x 
+        setmatrix<- function(solve) m <<- solve(x) #assign solve to m
+        getmatrix <- function() m #get m
         list(set = set, get = get,
              setmatrix = setmatrix,
              getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## `cacheSolve`: This function computes the inverse of the special
+"matrix" returned by `makeCacheMatrix` above. If the inverse has
+already been calculated (and the matrix has not changed), then
+`cacheSolve` should retrieve the inverse from the cache.
 
-cacheSolve <- function(matrix, ...) {
-        m <- x$getmatrix()
-        if(!is.null(m)) {
-                message("getting cached data")
+cacheSolve <- function(x, ...) {
+        m <- getmatrix()
+        if(!is.null(m)) { # if m exsit
+                message("getting cached data") # get the value of m
                 return(m)
         }
-        data <- x$get()
+        data <- get()#get x
         m <- solve(data, ...)
-        x$setmatrix(m)
+        setmatrix(m)
         m
 }
